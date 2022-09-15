a268b7d3d542e4c8c9f68c407f42f1d6f80031e1
JustKingYT1 <112751229+JustKingYT1@users.noreply.github.com>
2022-09-15 20:19:33 +0300

    Lab 2, Waiting for verification, 15.09.2022

--- Elevator.py
+++ Elevator.py
@@ -1,64 +1,75 @@
 import logging
 
+
 class TypeUniversalException(Exception):
     pass
 
 
 class Elevator:
-    def __init__(self, floors_count, elevator_location, elevator_capacity):
+    def __init__(self, floors_count, elevator_capacity):
         self.floors_count = floors_count
-        self.elevator_location = elevator_location
+        self.elevator_location = 1
         self.elevator_capacity = elevator_capacity
-        self.floor_weigth = []
-        self.weigth_units = 0
 
-    def add_unit(self, floor_weigth: list):
-        self.floor_weigth.append(floor_weigth)
-        self.weigth_units += floor_weigth[1]
-        try:
-            for i in range(100):
-                print(f"Зашел человек!\n\nЭтаж на котором находится лифт: {self.elevator_location}\nЭтаж на котором человек выйдет: {self.floor_weigth[i][i]}\nВес: {self.floor_weigth[i][i+1]}\nОбщий вес лифта: {self.weigth_units}\n")
-        except:
-            return None
-    def elevator_call(self, calling_char):
-        CALLING_CHAR = "Y"
-        return calling_char == CALLING_CHAR
+    def move(self, number_floor):
+        if human.weigth_units <= self.elevator_capacity:
+            if number_floor <= self.floors_count:
+                self.elevator_location = number_floor
+                print(f'Мы прибыли на {number_floor} этаж\n')
+            else:
+                logging.critical("A message of CRITICAL severity")
+                raise TypeUniversalException("Указанный этаж находится вне диапазона этажей этого дома")
+        else:
+            logging.warning("A WARNING")
+            raise TypeUniversalException("Вес выше допустимого")
+
+    def __str__(self):
+        return f"Этаж на котором находится лифт: {self.elevator_location}\nОбщий вес лифта: {human.weigth_units}\nКоличество людей в лифте: {human.count_units}\n"
+
+
+class Unit:
+
+    def __init__(self):
+        self.units = []
+        self.weigth_units = 0
+        self.count_units = 0
+
+    def add_unit(self, unit: list):
+        self.units.append(unit)
+        self.weigth_units += unit[1]
+        self.count_units += 1
+        print(f"Зашел человек!\n\nЭтаж на котором человек выйдет: {unit[0]}\nВес: {unit[1]}\n")
 
     def to_ride(self, number_floor):
-        if self.elevator_call:
-            if self.weigth_units <= self.elevator_capacity:
-                if number_floor <= self.floors_count:
-                    self.elevator_location = number_floor
-                    print(f'Мы прибыли на {number_floor} этаж\n')
-                else:
-                    logging.critical("A message of CRITICAL severity")
-                    raise TypeUniversalException("Указанный этаж находится вне диапазона этажей этого дома")
-            else:
-                logging.warning("A WARNING")
-                raise TypeUniversalException("Вес выше допустимого")
+        elev.move(number_floor)
+        try:
+            for i in range(human.count_units):
+                try:
+                    human.delete_unit()
+                except:
+                    pass
+        except:
+            pass
 
     def delete_unit(self, i=0):
-        try:
-            while i < 10:
-                if self.elevator_location == self.floor_weigth[i][i]:
-                    self.weigth_units -= self.floor_weigth[i][i+1]
-                    self.floor_weigth.pop(i)
-                    print("Человек вышел!\n")
-                i +=1
-        except:
-            return None
+        while i <= self.count_units:
+            if elev.elevator_location == self.units[i][0]:
+                self.count_units -= 1
+                self.weigth_units -= self.units[i][1]
+                self.units.pop(i)
+                print("Человек вышел!\n")
+            i += 1
 
 
 if __name__ == "__main__":
-    elev = Elevator(10, 1, 120)
-    elev.elevator_call("Y")
-    elev.to_ride(3)
-    (elev.add_unit([5, 46]))
-    elev.to_ride(5)
-    elev.delete_unit()
-    (elev.add_unit([6, 45]))
-    elev.to_ride(6)
-    elev.delete_unit()
-    (elev.add_unit([3, 55]))
-    elev.to_ride(100)
-    elev.delete_unit()
+    elev = Elevator(10, 1200)
+    human = Unit()
+    human.to_ride(3)
+    human.add_unit([5, 78])
+    human.add_unit([2, 65])
+    human.add_unit([5, 34])
+    print(elev)
+    human.to_ride(5)
+    print(elev)
+    human.to_ride(2)
+    print(elev)
